fc4145da059c5c69a18a5106ab6be176b1c423b5
TalosCD <122673838+TalosCD@users.noreply.github.com>
2023-04-01 16:12:29 +0300

    Add files via upload

--- project_script.js
+++ project_script.js
@@ -2,6 +2,7 @@ let API_KEY = "zcohruuzqcb2s7nldlhhw2yl05hflden";
 let API_SECRET = "prvvpnyjv0ygmbflx2uxh6vjzpek0gqk";
 let STATION_ID = "147952";
 let t = Math.floor(Date.now() / 1000);
+const select = document.getElementById('station');
 
 let firstDateString = "2023-02-11T14:45:00";
 let secondDateString = "2023-02-11T16:00:00";
@@ -18,11 +19,19 @@ let apiHashHis = `api-key${API_KEY}end-timestamp${unixTimeInSecondsSecond}start-
 var apiSignatureHis = CryptoJS.HmacSHA256(apiHashHis, API_SECRET).toString();
 
 let FULL_URL_HIS = `https://api.weatherlink.com/v2/historic/${STATION_ID}?api-key=${API_KEY}&t=${t}&start-timestamp=${unixTimeInSecondsFirst}&end-timestamp=${unixTimeInSecondsSecond}&api-signature=${apiSignatureHis}`;
-
+var myChart = null;
 // const charData =
 //   "https://www.googleapis.com/drive/v3/files/1ABMSqR_CFUq5bFYpQjsb64g2rG1ohXw5?alt=media&key=AIzaSyAHxeiBSCVs4eclFBkilxqdSzGniaFQ_Rw";
 
-fetch(FULL_URL_HIS)
+select.addEventListener('change', function handleChange(event) {
+  console.log(event.target.value); // 👉️ get selected VALUE
+  STATION_ID = event.target.value;
+  t = Math.floor(Date.now() / 1000);
+  apiHashHis = `api-key${API_KEY}end-timestamp${unixTimeInSecondsSecond}start-timestamp${unixTimeInSecondsFirst}station-id${STATION_ID}t${t}`;
+  apiSignatureHis = CryptoJS.HmacSHA256(apiHashHis, API_SECRET).toString();
+  FULL_URL_HIS = `https://api.weatherlink.com/v2/historic/${STATION_ID}?api-key=${API_KEY}&t=${t}&start-timestamp=${unixTimeInSecondsFirst}&end-timestamp=${unixTimeInSecondsSecond}&api-signature=${apiSignatureHis}`;
+
+  fetch(FULL_URL_HIS)
   .then((rep) => rep.json())
   .then((data) => {
     data = data.sensors[1].data;
@@ -41,7 +50,11 @@ fetch(FULL_URL_HIS)
     console.log(date);
     // setup
     const ctx = document.getElementById("myChart").getContext("2d");
-    const myChart = new Chart(ctx, {
+
+    if(myChart != null){
+      myChart.destroy();
+    }
+    myChart = new Chart(ctx, {
       type: "line",
       data: {
         labels: date,
@@ -68,6 +81,9 @@ fetch(FULL_URL_HIS)
       },
     });
   });
+});
+
+
 
 // document.getElementById("tableContainerHis").innerHTML = table;
 
